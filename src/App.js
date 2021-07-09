21dc47f7fb14dfa9a3dde7c08c3afac2d08f994d
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2021-07-09 18:19:45 +0530

    added signin signup pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import {  BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Signin from './Components/Signin/Signin';
+import Signup from './Components/Signup/Signup';
 
 function App() {
   return (
     <div className="App">
-      <h1>This is working fine.</h1>
+      
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Signin}/>
+          <Route exact path="/signup" component={Signup}/>
+        </Switch>
+      </Router>
+
     </div>
   );
 }
