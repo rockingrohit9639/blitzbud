6d19990464dcf77884242f4ad46e34fdaf51cc99
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2021-07-09 18:28:49 +0530

    added admin dashboard page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {  BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Signin from './Components/Signin/Signin';
 import Signup from './Components/Signup/Signup';
+import Dashboard from './Components/Admin/Dashboard';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Signin}/>
           <Route exact path="/signup" component={Signup}/>
+          <Route exact path="/dashboard" component={Dashboard}/>
         </Switch>
       </Router>
 
